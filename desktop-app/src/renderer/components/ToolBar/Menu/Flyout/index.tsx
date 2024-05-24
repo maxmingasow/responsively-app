3e0e22427218412731ea99287c7d381e3a01e448
Max Mingasow <maxmdev@icloud.com>
2024-05-24 11:42:27 +0200

    stylechange

--- desktop-app/src/renderer/components/ToolBar/Menu/Flyout/index.tsx
+++ desktop-app/src/renderer/components/ToolBar/Menu/Flyout/index.tsx
@@ -17,7 +17,7 @@ interface Props {
 
 const MenuFlyout = ({ closeFlyout }: Props) => {
   return (
-    <div className="absolute top-[26px] right-[4px] z-50 flex w-80 flex-col gap-2 rounded bg-white p-2 pb-0 text-sm shadow-lg ring-1 ring-slate-500 !ring-opacity-40 focus:outline-none dark:bg-slate-900 dark:ring-white dark:!ring-opacity-40">
+    <div className="absolute top-[26px] right-[4px] z-50 flex w-80 flex-col gap-2 rounded bg-[#EFF1F5] p-2 pb-0 text-sm shadow-lg ring-1 ring-slate-500 !ring-opacity-40 focus:outline-none dark:bg-slate-900 dark:ring-white dark:!ring-opacity-40">
       <Zoom />
       <PreviewLayout />
       <UITheme />
